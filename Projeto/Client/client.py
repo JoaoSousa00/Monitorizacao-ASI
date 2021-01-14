5fb1b85efc3f60578467a45e1bf8d1f2cc5d9f0e
Joao Sousa <8180175@estg.ipp.pt>
2021-01-14 10:57:18 +0000

    Mais desenvolvimento no servidor e cliente

--- Projeto/Client/client.py
+++ Projeto/Client/client.py
@@ -4,27 +4,54 @@ import time
 import requests
 from Client.Dados import UserMachine
 from Client.Logs import LogsTreatment
+from os import path
 
 
 def askToken():
+    # Verifica se o ficheiro existe
+    if(path.exists("key.txt")):
+        # Se existir apenas vamos buscar o token do utilizador e vamos mandar os dados ao servidor com esse
+        # identificador
 
-    ip = UserMachine.getIP()
+        f = open("key.txt", "r")
+        token = f.readline()
+        f.close()
+    else:
+        # Se não existir vai mandar um pedido ao servidor para que este o guarde e retorne uma key que nos identificará
 
-    so = UserMachine.getSO()
+        # ip da máquina
+        ip = UserMachine.getIP()
 
-    data = {"ip": ip, "so": so}
+        # sistema operativo da máquina
+        so = UserMachine.getSO()
 
-    os.system('python3 Logs/apache-fake-log-gen.py -n 100 -o LOG -p Logs/StoredLogs/')
+        data = {"ip": ip, "so": so}
 
-    print(LogsTreatment.getLogsInfo("Logs/StoredLogs"))
+        print(data)  # TIRARRRRRRRRRRR
 
-    # Enviar os dados para o servidor usando o WebService
-    res = requests.post('http://localhost:5000/setData', json=data)
+        # Enviar os dados da máquina para o servidor usando o WebService
+        res = requests.post('http://localhost:5000/setData', json=data)
 
-    if res.ok:
-        print(res.json())
+        if res.ok:
+            print(res.json())
+            token = res.json()["key"]
+
+            f = open("key.txt", "w")
+            f.write(token)
+            f.close()
+
+            ### Mandar dados
+        else:
+            # Se a resposta não for ok então não mandamos os dados
+            return
+
+    # Gerar logs
+    os.system('python3 Logs/apache-fake-log-gen.py -n 10 -o LOG -p Logs/StoredLogs/')
+
+    # Calcular as expressões do log
+    print(LogsTreatment.getLogsInfo("Logs/StoredLogs"))  # TIRARRRRRRRRRRR
 
 
 while True:
     askToken()
-    time.sleep(300) # Adormece o cliente por 5 minutos e depois manda mais dados
+    time.sleep(300)  # Adormece o cliente por 5 minutos e depois manda mais dados
--- Projeto/Server/webService.py
+++ Projeto/Server/webService.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from flask import Flask, request
 from tinydb import TinyDB
 from datetime import datetime
@@ -8,21 +10,44 @@ app = Flask(__name__)
 dbData = TinyDB('DataBase/data')  # Guarda os dados das máquinas
 
 
+@app.route('/configuration', methods=['POST'])  # GET requests will be blocked
+def genKey():
+    req_data = request.get_json()
+
+    # Informações da máquina que fez o pedido
+    ip = req_data['ip']
+    so = req_data['so']
+
+    # cria uma string que vai ser posteriormente codificada. Esta string contem a data atual e ip e so recebidos
+    secret = str(datetime.now()) + ip + so
+
+    # A key é feita usando sha512
+    key = hashlib.sha512(secret.encode()).hexdigest()
+
+    # Guarda-se os dados da máquina na Base de Dados
+    dbData.insert({"key": key, "IP": ip, "SO": so})  # # # COMO GUARDAR???
+
+    # Find ALL
+    print(dbData.all())
+
+    return {"status": str(201), "key": key}
+
+
 @app.route('/setData', methods=['POST'])  # GET requests will be blocked
 def setData():
     req_data = request.get_json()
 
-    total_mem = req_data['total']
-    free_mem = req_data['free']
+    ip = req_data['ip']
+    so = req_data['so']
 
     ins = {}
     ins['measurement'] = "memory"
     ins["tags"] = req_data
     ins["time"] = datetime.now()
-    ins["fields"] = {"value": total_mem}
+    ins["fields"] = {"value": ip}
     json_body = [ins]
 
-    dbData.insert({"total": total_mem})
+    dbData.insert({"ip": ip})
 
     # Find ALL
     print(dbData.all())
